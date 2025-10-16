2b0ff43e2ea5d6e4f00ad61d1f25a8a1334447eb
Arkatul <alexis.stap@outlook.com>
2025-10-16 16:18:09 +0200

    Add optional drop flag and rebuild logic to init script

--- init.sql
+++ init.sql
@@ -1,3 +1,58 @@
+DECLARE @DropAndRecreate BIT = 0;
+
+IF @DropAndRecreate = 1
+BEGIN
+    IF OBJECT_ID('dbo.vw_DatabaseSessionTotalsByPrincipal', 'V') IS NOT NULL
+        DROP VIEW dbo.vw_DatabaseSessionTotalsByPrincipal;
+
+    IF OBJECT_ID('dbo.vw_DatabaseSessionDailyTotalsByPrincipal', 'V') IS NOT NULL
+        DROP VIEW dbo.vw_DatabaseSessionDailyTotalsByPrincipal;
+
+    IF OBJECT_ID('dbo.vw_DatabaseSessionTotalsByDatabase', 'V') IS NOT NULL
+        DROP VIEW dbo.vw_DatabaseSessionTotalsByDatabase;
+
+    IF OBJECT_ID('dbo.usp_CaptureSessionSnapshots', 'P') IS NOT NULL
+        DROP PROCEDURE dbo.usp_CaptureSessionSnapshots;
+
+    IF OBJECT_ID('dbo.CapturedSessions', 'U') IS NOT NULL
+        DROP TABLE dbo.CapturedSessions;
+
+    IF OBJECT_ID('dbo.DatabaseSessionHourly', 'U') IS NOT NULL
+        DROP TABLE dbo.DatabaseSessionHourly;
+END;
+GO
+
+IF OBJECT_ID('dbo.DatabaseSessionHourly', 'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.DatabaseSessionHourly
+    (
+        Id INT IDENTITY(1,1) NOT NULL,
+        SnapshotDate DATE NOT NULL,
+        SnapshotHour TINYINT NOT NULL,
+        DatabaseName SYSNAME NOT NULL,
+        HostName NVARCHAR(128) NOT NULL,
+        LoginName NVARCHAR(128) NOT NULL,
+        SessionCount INT NOT NULL,
+        CONSTRAINT PK_DatabaseSessionHourly PRIMARY KEY CLUSTERED (Id)
+    );
+
+    CREATE UNIQUE NONCLUSTERED INDEX IX_DatabaseSessionHourly_UQ
+        ON dbo.DatabaseSessionHourly (SnapshotDate, SnapshotHour, DatabaseName, HostName, LoginName);
+END;
+GO
+
+IF OBJECT_ID('dbo.CapturedSessions', 'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.CapturedSessions
+    (
+        session_id INT NOT NULL,
+        login_time DATETIME2(0) NOT NULL,
+        SnapshotHour DATETIME2(0) NOT NULL,
+        CONSTRAINT PK_CapturedSessions PRIMARY KEY CLUSTERED (session_id, login_time, SnapshotHour)
+    );
+END;
+GO
+
 SET ANSI_NULLS ON;
 GO
 SET QUOTED_IDENTIFIER ON;
@@ -9,10 +64,15 @@ BEGIN
     SET NOCOUNT ON;
 
     DECLARE @capture_time DATETIME2(0) = SYSDATETIME();
+    DECLARE @snapshot_hour DATETIME2(0) = DATEADD(HOUR, DATEDIFF(HOUR, 0, @capture_time), 0);
+
+    DELETE FROM dbo.CapturedSessions
+    WHERE SnapshotHour < @snapshot_hour;
 
     CREATE TABLE #Sessions
     (
         session_id INT NOT NULL,
+        login_time DATETIME2(0) NOT NULL,
         database_id INT NOT NULL,
         database_name SYSNAME NOT NULL,
         host_name NVARCHAR(128) NOT NULL,
@@ -20,9 +80,10 @@ BEGIN
         capture_time DATETIME2(0) NOT NULL
     );
 
-    INSERT INTO #Sessions (session_id, database_id, database_name, host_name, login_name, capture_time)
+    INSERT INTO #Sessions (session_id, login_time, database_id, database_name, host_name, login_name, capture_time)
     SELECT
         s.session_id,
+        s.login_time,
         s.database_id,
         DB_NAME(s.database_id) AS database_name,
         ISNULL(NULLIF(s.host_name, ''), 'UNKNOWN') AS host_name,
@@ -31,7 +92,19 @@ BEGIN
     FROM sys.dm_exec_sessions AS s
     WHERE s.is_user_process = 1
       AND s.database_id > 4
-      AND s.database_id IS NOT NULL;
+      AND s.database_id IS NOT NULL
+      AND NOT EXISTS
+      (
+          SELECT 1
+          FROM dbo.CapturedSessions AS cs
+          WHERE cs.session_id = s.session_id
+            AND cs.login_time = s.login_time
+            AND cs.SnapshotHour = @snapshot_hour
+      );
+
+    INSERT INTO dbo.CapturedSessions (session_id, login_time, SnapshotHour)
+    SELECT DISTINCT session_id, login_time, @snapshot_hour
+    FROM #Sessions;
 
     WITH Aggregated AS
     (
@@ -57,22 +130,6 @@ BEGIN
     WHEN NOT MATCHED BY TARGET THEN
         INSERT (SnapshotDate, SnapshotHour, DatabaseName, HostName, LoginName, SessionCount)
         VALUES (source.SnapshotDate, source.SnapshotHour, source.database_name, source.host_name, source.login_name, source.SessionCount);
-END;
-GO
-
-IF OBJECT_ID('dbo.DatabaseSessionHourly', 'U') IS NULL
-BEGIN
-    CREATE TABLE dbo.DatabaseSessionHourly
-    (
-        SnapshotDate DATE NOT NULL,
-        SnapshotHour TINYINT NOT NULL,
-        DatabaseName SYSNAME NOT NULL,
-        HostName NVARCHAR(128) NOT NULL,
-        LoginName NVARCHAR(128) NOT NULL,
-        SessionCount INT NOT NULL,
-        CONSTRAINT PK_DatabaseSessionHourly PRIMARY KEY CLUSTERED
-            (SnapshotDate, SnapshotHour, DatabaseName, HostName, LoginName)
-    );
 END;
 GO
 
